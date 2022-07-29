05a857f7fd9f06a4224f39f9b4591be025ad6671
luowenyan04 <luowenyan04@gmail.com>
2022-07-29 11:54:17 +0800

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component, VERSION } from '@angular/core';
+import { Message } from './message';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = '';
+  content = '';
+  messages: Message[] = [];
+
+  addMessage(): void {
+    if (!this.name.trim() || !this.content.trim()) {
+      return;
+    }
+
+    const message = new Message(this.name, this.content);
+    this.messages.push(message);
+    this.content = '';
+  }
 }
